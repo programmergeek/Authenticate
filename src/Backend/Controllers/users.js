79744b9ab42dc6afb70e176cc5096289af04e2f1
programmergeek <programergeek@gmail.com>
2021-09-06 14:12:06 +0200

    created users controller

---
+++ src/Backend/Controllers/users.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+exports.create_new_user = function (req, res, next) {
+  return;
+};
+
+exports.login_user = function (req, res, next) {
+  return;
+};
